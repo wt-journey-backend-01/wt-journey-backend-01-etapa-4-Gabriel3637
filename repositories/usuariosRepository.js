9df1509f9b7daac7199eb9be09d85842d54d96c2
Gabriel3637 <gabriellvbs14@gmail.com>
2025-08-28 17:18:16 -0300

    :sparkles: feat(usuarioRepository): Adicionando arquivo de acesso a tabela usuarios

---
+++ repositories/usuariosRepository.js
@@ -0,0 +1,59 @@
+const db = require('../db/db');
+
+async function read(id = null) {
+    try{
+        let resp;
+        if(id) {
+            resp = await db('usuarios').where({ id });
+            let isSingular = Array.isArray(resp) && resp.length === 1;
+            return isSingular ? resp[0] : null;
+        } else {
+            let resp = await db('usuarios').select('*');
+            return resp;
+        }
+    } catch(err) {
+        console.log(err);
+        return false;
+    }
+}
+
+async function create(usuario) {
+    try{
+        let resp = await db('usuarios').insert(usuario, ["*"]);
+        return resp[0];
+    }catch(err){
+        console.log(err);
+        return false;
+    }
+}
+
+async function update(id, usuario) {
+    try {
+        let resp = await db('usuarios').where({ id }).update(usuario, ["*"]);
+        if(resp.length == 0){
+            return null;
+        } else {
+            return resp[0];
+        }
+    }catch(err){
+        console.log(err);
+        return false;
+    }
+}
+
+async function remove(id) {
+    try {
+        let resp = await db('usuarios').where({ id }).del();
+        return resp;
+    }catch(err){
+        console.log(err);
+        return false;
+    }
+}
+
+module.exports = {
+    read,
+    create,
+    update,
+    remove
+};
